9539cbe874332367be6abf542ec86a6aaf176323
Alfredo Sumaran <a.sumaran@gmail.com>
2022-01-06 23:47:59 -0500

    Ability to pay using a credit card

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./index.scss";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || ""
+);
 
 function App() {
-  return <div className="app">Hello World!</div>;
+  const options = {
+    clientSecret: process.env.REACT_APP_STRIPE_CLIENT_SECRET,
+  };
+
+  return (
+    <div className="app">
+      <Elements stripe={stripePromise} options={options}>
+        <CheckoutForm />
+      </Elements>
+    </div>
+  );
 }
 
 export default App;
